1ccff1413eb90aaccf973dd39c2a0199651225d2
alstrasz <lutsai2000@ya.ru>
2021-12-14 17:12:21 +0300

    attemt to fix

--- front/src/app/insert/insert.component.ts
+++ front/src/app/insert/insert.component.ts
@@ -30,4 +30,12 @@ export class InsertComponent implements OnInit {
                 args: this.args,
             } );
     }
+
+    number_only ( event: any ): boolean {
+        const charCode = ( event.which ) ? event.which : event.keyCode;
+        if ( charCode > 31 && ( charCode < 48 || charCode > 57 ) ) {
+            return false;
+        }
+        return true;
+    }
 }
